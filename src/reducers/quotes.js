ad6651d62b4bcf8ef1c43e62abb4b3264c86c562
Heather Christman <efl7a@yahoo.de>
2018-08-10 11:21:41 -0400

    changed reducer to use slice so that quotes would remain in same order even after voting.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,6 +1,6 @@
 
 export default (state = [], action) => {
-  let quote, quotes, newQuote, votes, qIndex
+  let newQuote, qIndex
   switch(action.type){
 
     case 'ADD_QUOTE':
@@ -10,24 +10,28 @@ export default (state = [], action) => {
       return state.filter(quote => quote.id !== action.quoteId);
 
     case 'UPVOTE_QUOTE':
-      // qIndex = state.findIndex(quote => quote.id === action.quoteId);
-      // console.log(qIndex)
-      // newQuote = {...state[qIndex], votes: state[qIndex].votes +1 }
-      // console.log(newQuote)
-      // console.log([...state.slice(0, qIndex -1), newQuote, state.slice(qIndex +1)])
-      // return [...state.slice(0, qIndex - 1), newQuote, state.slice(qIndex +1)]
-      quote = state.filter(quote => quote.id === action.quoteId);
-      quotes = state.filter(quote => quote.id !== action.quoteId);
-      newQuote = {...quote[0], votes: quote[0].votes + 1}
-      return [...quotes, newQuote];
+      qIndex = state.findIndex(quote => quote.id === action.quoteId);
+      console.log(qIndex)
+      newQuote = {...state[qIndex], votes: state[qIndex].votes +1 }
+      console.log(newQuote)
+      console.log([...state.slice(0, qIndex), newQuote, ...state.slice(qIndex +1)])
+      return [...state.slice(0, qIndex), newQuote, ...state.slice(qIndex +1)]
+      // quote = state.filter(quote => quote.id === action.quoteId);
+      // quotes = state.filter(quote => quote.id !== action.quoteId);
+      // newQuote = {...quote[0], votes: quote[0].votes + 1}
+      // return [...quotes, newQuote];
 
     case 'DOWNVOTE_QUOTE':
     console.log(action.quoteId)
-      quote = state.filter(quote => quote.id === action.quoteId);
-      if(quote[0].votes > 0) {
-        quotes = state.filter(quote => quote.id !== action.quoteId);
-        newQuote = {...quote[0], votes: quote[0].votes - 1}
-        return [...quotes, newQuote];
+      // quote = state.filter(quote => quote.id === action.quoteId);
+      // if(quote[0].votes > 0) {
+      //   quotes = state.filter(quote => quote.id !== action.quoteId);
+      //   newQuote = {...quote[0], votes: quote[0].votes - 1}
+      //   return [...quotes, newQuote];
+      qIndex = state.findIndex(quote => quote.id === action.quoteId);
+      if(state[qIndex].votes > 0) {
+        newQuote = {...state[qIndex], votes: state[qIndex].votes - 1 }
+        return [...state.slice(0, qIndex), newQuote, ...state.slice(qIndex +1)]
       } else {
         return state;
       }
