257893c19f9e8d366b4c98bd805fa2ada806e837
Jeremy Parker <jrobinparker@gmail.com>
2019-03-19 00:14:05 +0000

    WIP

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
-export default (state = [], action) => {
-  return state;
+function quotesReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      return {
+        ...state,
+        [action.quote.id]: {
+          ...quote,
+          votes: quote[votes].length + 1
+        }
+      }
+    default:
+      return state
+  }
 }
+
+export default quotesReducer
